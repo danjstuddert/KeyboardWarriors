0f4e0be5070d9ba8979e1ca50098d50c0a8cf57c
danjstuddert <danieljstuddert@gmail.com>
2017-08-01 23:35:39 +1000

    Keyboard setup

--- Assets/Scripts/Keys/KeyController.cs
+++ Assets/Scripts/Keys/KeyController.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyController : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		for (int i = 0; i < transform.childCount; i++) {
+			//Gizmos.DrawSphere(transform.GetChild(i).position, 0.1f);
+		}
 	}
 }
